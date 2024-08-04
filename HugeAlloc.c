c41161774cbfc5c2547580e55c639243621dc433
Md-Monowarul-Amin <aminsaraj2015@gmail.com>
2024-08-04 20:22:48 +0600

    started

--- HugeAlloc.c
+++ HugeAlloc.c
@@ -21,12 +21,16 @@ int do_alloc = 1;
 
 void * HugeAlloc(ssize_t size){
 
-    if (size >= ONE_MB || current_AT_index == 0 || do_alloc == 1){
+
+    if (current_AT_index == 0 || do_alloc == 1){
         // struct PageAllocation * temp_allocation;
         // allocation_table[current_AT_index] = temp_allocation;
         current_AT_index += 1;
         do_alloc = 0;
 
+        
+
+        printf("PID HugeAlloc: %d", getpid());
 
         // int brk()
 
@@ -75,94 +79,115 @@ void * HugeAlloc(ssize_t size){
         }
         else{
             do_alloc = 1;
+            
             return HugeAlloc(size);
         }
     }
 }
 
-int main(){
 
-
-    // struct PageAllocation *temp_huge;
-    // temp_huge->total_size = 0;
-
-    int malloc_size_i = 1000000;
-    int malloc_size_j = 700;
-
-    int brk_size_i = 1000000;
-    int brk_size_j = 700;
-
-    clock_t start_mlk = clock();
-
-    clock_t time_for_allocation_start_mlk = clock();
-
-    int * arr_malloc = malloc(malloc_size_i * sizeof(int));
-
-    clock_t time_for_allocation_end_mlk = clock();
-
-    printf("Time for allocation of mlk: %f \n", (double) (time_for_allocation_end_mlk - time_for_allocation_start_mlk));
-
-
-    for(int i=0; i < malloc_size_i; i++){
-        // int * arr = HugeAlloc(1);
-        
-        int * temp_arr = malloc(malloc_size_j * sizeof(int));
-
-        for(int j = 0; j < malloc_size_j; j++){
-            temp_arr[j] = 1;
+void * alloc(ssize_t size){
+    if(current_AT_index != 0 || do_alloc == 0){
+         if((next_ret_ptr + size - current_break) <= TWO_MB){
+            
+            // printf("Achieved : %ld", (next_ret_ptr + size - current_break));
+            current_ret_ptr = next_ret_ptr;
+            next_ret_ptr += size;
+            return current_ret_ptr; 
         }
-        arr_malloc[i] = (int *) temp_arr;
-        // arr[10000] = 2;
-    }
-
-    clock_t end_mlk = clock();
-
-    printf("time for mlk: %f \n", (double)(end_mlk - start_mlk));
-
-
-
-
-
-    // printf("%p", arr);
-
-    clock_t start_brk = clock();
-
-    clock_t time_for_allocation_start_brk = clock();
-
-    int * arr = HugeAlloc(brk_size_i * sizeof(int));
-
-    clock_t time_for_allocation_end_brk = clock();
-
-    printf("Time for allocation of brk: %ld \n", (time_for_allocation_end_brk - time_for_allocation_start_brk));
-
-
-    for(int i=0; i < brk_size_i; i++){
-
-        int * temp_arr =  HugeAlloc(brk_size_j  * sizeof(int));
-
-        for(int j = 0; j < brk_size_j; j++){
-            temp_arr[j] = 1;
+        else{
+            do_alloc = 1;
+            
+            return HugeAlloc(size);
         }
-        arr[i] = temp_arr;
-
-        // int * arr = HugeAlloc(1);
-        // arr[10000] = 2;
     }
-
-    clock_t end_brk = clock();
-
-    printf("time for brk: %f \n", (double)(end_brk - start_brk));
-
-
-
-
-
-    // printf("%d", *arr);
-
-
-
-
-    printf("arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
-
-    return 0;
 }
+
+
+// int main(){
+
+
+    // // struct PageAllocation *temp_huge;
+    // // temp_huge->total_size = 0;
+
+    // int malloc_size_i = 1000000;
+    // int malloc_size_j = 700;
+
+    // int brk_size_i = 1000000;
+    // int brk_size_j = 700;
+
+    // clock_t start_mlk = clock();
+
+    // clock_t time_for_allocation_start_mlk = clock();
+
+    // int * arr_malloc = malloc(malloc_size_i * sizeof(int));
+
+    // clock_t time_for_allocation_end_mlk = clock();
+
+    // printf("Time for allocation of mlk: %f \n", (double) (time_for_allocation_end_mlk - time_for_allocation_start_mlk));
+
+
+    // for(int i=0; i < malloc_size_i; i++){
+    //     // int * arr = HugeAlloc(1);
+        
+    //     int * temp_arr = malloc(malloc_size_j * sizeof(int));
+
+    //     for(int j = 0; j < malloc_size_j; j++){
+    //         temp_arr[j] = 1;
+    //     }
+    //     arr_malloc[i] = (int *) temp_arr;
+    //     // arr[10000] = 2;
+    // }
+
+    // clock_t end_mlk = clock();
+
+    // printf("time for mlk: %f \n", (double)(end_mlk - start_mlk));
+
+
+
+
+
+    // // printf("%p", arr);
+
+    // clock_t start_brk = clock();
+
+    // clock_t time_for_allocation_start_brk = clock();
+
+    // int * arr = HugeAlloc(brk_size_i * sizeof(int));
+
+    // clock_t time_for_allocation_end_brk = clock();
+
+    // printf("Time for allocation of brk: %ld \n", (time_for_allocation_end_brk - time_for_allocation_start_brk));
+
+
+    // for(int i=0; i < brk_size_i; i++){
+
+    //     int * temp_arr =  HugeAlloc(brk_size_j  * sizeof(int));
+
+    //     for(int j = 0; j < brk_size_j; j++){
+    //         temp_arr[j] = 1;
+    //     }
+    //     arr[i] = temp_arr;
+
+    //     // int * arr = HugeAlloc(1);
+    //     // arr[10000] = 2;
+    // }
+
+    // clock_t end_brk = clock();
+
+    // printf("time for brk: %f \n", (double)(end_brk - start_brk));
+
+
+
+
+
+    // // printf("%d", *arr);
+
+
+
+
+    // printf("arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+
+    // printf("Pid_Main: %d", getpid());
+    // return 0;
+// }
--- temp.c
+++ temp.c
@@ -1,29 +1,113 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "HugeAlloc.c"
 
 
-struct temp
-{
-    /* data */
-};
 
 
 
 
 int main(){
-    int n = 10;
-    int *marks;
+    // int n = 10;
+    // int *marks;
 
-    marks = (int *) malloc(n * sizeof(int));
+    // marks = (int *) malloc(n * sizeof(int));
 
-    for (int i=0; i<10; i++){
-        marks[i] = i+1;
-    }
+    // for (int i=0; i<10; i++){
+    //     marks[i] = i+1;
+    // }
 
-    for(int i=0; i< 10; i++){
-        printf("Marks %d", marks[i]);
-    }
-    printf("\n");
+    // for(int i=0; i< 10; i++){
+    //     printf("Marks %d", marks[i]);
+    // }
+    // printf("\n");
     
+    // return 0;
+
+
+        // struct PageAllocation *temp_huge;
+    // temp_huge->total_size = 0;
+
+    int malloc_size_i = 1000000;
+    int malloc_size_j = 700;
+
+    int brk_size_i = 1000000;
+    int brk_size_j = 700;
+
+    clock_t start_mlk = clock();
+
+    clock_t time_for_allocation_start_mlk = clock();
+
+    int * arr_malloc = malloc(malloc_size_i * sizeof(int));
+
+    clock_t time_for_allocation_end_mlk = clock();
+
+    printf("Time for allocation of mlk: %f \n", (double) (time_for_allocation_end_mlk - time_for_allocation_start_mlk));
+
+
+    for(int i=0; i < malloc_size_i; i++){
+        // int * arr = HugeAlloc(1);
+        
+        int * temp_arr = malloc(malloc_size_j * sizeof(int));
+
+        for(int j = 0; j < malloc_size_j; j++){
+            temp_arr[j] = 1;
+        }
+        arr_malloc[i] = (int *) temp_arr;
+        // arr[10000] = 2;
+    }
+
+    clock_t end_mlk = clock();
+
+    printf("time for mlk: %f \n", (double)(end_mlk - start_mlk));
+
+
+
+
+
+    // printf("%p", arr);
+
+    clock_t start_brk = clock();
+
+    clock_t time_for_allocation_start_brk = clock();
+
+    int * arr = HugeAlloc(brk_size_i * sizeof(int));
+
+    clock_t time_for_allocation_end_brk = clock();
+
+    printf("Time for allocation of brk: %ld \n", (time_for_allocation_end_brk - time_for_allocation_start_brk));
+
+
+    for(int i=0; i < brk_size_i; i++){
+
+        int * temp_arr =  HugeAlloc(brk_size_j  * sizeof(int));
+
+        for(int j = 0; j < brk_size_j; j++){
+            temp_arr[j] = 1;
+        }
+        arr[i] = temp_arr;
+
+        // int * arr = HugeAlloc(1);
+        // arr[10000] = 2;
+    }
+
+    clock_t end_brk = clock();
+
+    printf("time for brk: %f \n", (double)(end_brk - start_brk));
+
+
+
+
+
+    // printf("%d", *arr);
+
+
+
+
+    printf("arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+
+    printf("Pid_Main: %d", getpid());
     return 0;
+
 }
